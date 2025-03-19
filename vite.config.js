24a1290cb275bc25153a076a2813effddc300c6c
方海生 <2386659195@qq.com>
2025-03-19 22:20:34 +0800

    更新

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig((config) => {
   return {
-    base: config.command === 'serve' ? './' : '/app-collection/',
+    // base: config.command === 'serve' ? './' : '/app-collection/',
+    base: './',
     plugins: [vue()],
     resolve: {
       alias: {
