c08ad18091e4354eabfe9906c403f6ce80d60421
方海生 <2386659195@qq.com>
2025-03-19 21:50:45 +0800

    更新

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/app-collection/',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
+export default defineConfig((config) => {
+  return {
+    base: config.command === 'serve' ? './' : '/app-collection/',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
     }
   }
 })
